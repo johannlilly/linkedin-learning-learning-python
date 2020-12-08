8cf5a87e958474da0d5af2d7ba0cc33519f4a5ec
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2020-12-08 12:58:25 -0500

    vs code is configured for debugging but doesn't accept user input with this configuration

--- exercise-files/Ch2/helloworld_start.py
+++ exercise-files/Ch2/helloworld_start.py
@@ -3,9 +3,9 @@
 #
 
 def main():
-  print("Hello World")
-  name = input("What is your name? ")
-  print("Nice to meet you,", name)
+  print("hello world")
+  # name = input("What is your name? ")
+  # print("Nice to meet you,", name)
 
 # you are stating that this file is its own program, and that you want to run the code when called
 # this is in contrast to if you include this module in another program, where you want to control when this code is executed
