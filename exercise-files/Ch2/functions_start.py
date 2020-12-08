7e584616aec6aad27c061aa70a55da8fc90d3a3f
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2020-12-08 13:29:17 -0500

    functions
    - define a basic function
    - function that takes arguments
    - function that returns a value
    - function with default value for an argument
    - function with variable number of arguments

--- exercise-files/Ch2/functions_start.py
+++ exercise-files/Ch2/functions_start.py
@@ -3,17 +3,46 @@
 #
 
 # define a basic function
-
+def func1():
+  print("I am a function")
 
 # function that takes arguments
-
+def func2(arg1, arg2):
+  print(arg1, " ", arg2)
 
 # function that returns a value
-
+def cube(x):
+  return x*x*x
 
 # function with default value for an argument
+def power(num, x=1):
+  result = 1
+  for i in range(x):
+    result = result * num
+  return result
+
+#function with variable number of arguments (must be last parameter)
+def multi_add(*args):
+  result = 0
+  for x in args:
+    result = result + x
+  return result
 
 
-#function with variable number of arguments
+func1() # >>> I am a function
+print (func1()) # >>> I am a function \n None
+print (func1) # >>> <function func1 at 0x10718a4c0>
+# functions are objects
 
+func2(10,20) # >>> 10 20
+print (func2(10,20)) # >>> 10 20 \n None
 
+print (cube(3)) # >>> 27
+
+print (power(2)) # >>> 2
+print (power(2,3)) # >>> 8
+print (power(x=3, num=2)) # >>> 8
+# call functions using their named parameters
+
+print (multi_add(4,5,10,4)) # >>> 23
+print (multi_add(1,2,3,4,5,6,7)) # >>> 28
