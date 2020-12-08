63f292374f2168a1e267dc67711b5e0c79d10880
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2020-12-08 13:05:25 -0500

    variables, global, del

--- exercise-files/Ch2/variables_start.py
+++ exercise-files/Ch2/variables_start.py
@@ -3,17 +3,37 @@
 #
 
 # Declare a variable and initialize it
-
+f=0
+print(f)
 
 
 # re-declaring the variable works
-
+f="abc"
+print(f)
 
 
 # ERROR: variables of different types cannot be combined
-
+# python is a strongly-typed language, even though you don't have to declare the variable type before you use it
+# type is inferred
+print("this is a string" + str(123))
 
 
 # Global vs. local variables in functions
+def someFunction():
+  f="def"
+  print(f)
 
+someFunction() # >>> def
+print(f) # >>> abc
+
+def someFunction():
+  global f
+  f="def"
+  print(f)
+
+someFunction() # >>> def
+print(f) # >>> def
+
+del f # delete definition of variable previously declared
+print(f) # >>> "NameError: name 'f' is not defined"
 
