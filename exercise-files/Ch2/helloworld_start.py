6cf6cd246540368584800bdd534b3d18857f2ecc
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2020-12-08 12:48:37 -0500

    first function; main

--- exercise-files/Ch2/helloworld_start.py
+++ exercise-files/Ch2/helloworld_start.py
@@ -2,6 +2,12 @@
 # Example file for HelloWorld
 #
 
-print("Hello World")
-name = input("What is your name? ")
-print("Nice to meet you,", name)
+def main():
+  print("Hello World")
+  name = input("What is your name? ")
+  print("Nice to meet you,", name)
+
+# you are stating that this file is its own program, and that you want to run the code when called
+# this is in contrast to if you include this module in another program, where you want to control when this code is executed
+if __name__ == "__main__":
+  main()
